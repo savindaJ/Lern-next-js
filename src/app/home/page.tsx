5ffd03d3b6d49240c717263a06298a75d3af708a
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-04-05 23:38:47 +0530

    Update CSS file and add new cards to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import './../home/inde.css';
+import './../home/index.css';
 
 const HomePage = () => {
     return (
@@ -19,6 +19,18 @@ const HomePage = () => {
                         </div>
                         <div className="row-span-3 border home-card bg-cyan-700 rounded-md">
                             <h3 className="text-4xl font-bold mt-4 text-white">Total Of Orders</h3>
+                            <h2 className='text-4xl font-bold mt-10'>10</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="container m-auto mt-10">
+                    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+                        <div className="row-span-3 border home-card bg-cyan-700 rounded-md">
+                            <h3 className="text-4xl font-bold mt-4 text-white">Total Of Customer</h3>
+                            <h2 className='text-4xl font-bold mt-10'>10</h2>
+                        </div>
+                        <div className="row-span-3 border home-card bg-cyan-700	 rounded-md">
+                            <h3 className="text-4xl font-bold mt-4 text-white">Total Of Items</h3>
                             <h2 className='text-4xl font-bold mt-10'>10</h2>
                         </div>
                     </div>

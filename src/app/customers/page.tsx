f3bd1faef260b41f3d224f87b0c20dc5c0939f3e
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-04-06 00:24:04 +0530

    Add modal dialog and customer table to CustomersPage

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,11 +1,76 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import dynamic from 'next/dynamic';
+
+
+
+
+function openModel() {
+    const modal = document.getElementById('my_modal_1') as HTMLElement;
+    (modal as HTMLDialogElement).showModal();
+}
 
 const CustomersPage = () => {
     return (
         <div>
             <NavBar />
-            <h1 className="text-4xl font-bold text-center mt-10">Customers Page</h1>
+
+            <section className='container mt-10 flex justify-end'>
+            </section>
+
+            <section className='container m-auto mt-10'>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr className='hover:bg-sky-700'>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+                            {/* row 2 */}
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Purple</td>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>3</th>
+                                <td>Brice Swyre</td>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+
+
+            <dialog id="my_modal_1" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click the button below to close</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
